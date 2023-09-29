1ee80dee1f50b683263678f4f1de008088a32c82
kourosh-H <kourosh.hajivand0@gmail.com>
2023-09-29 21:13:46 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { motion } from "framer-motion";
+import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const constraintsRef = useRef(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="w-full min-h-screen bg-white flex items-center justify-center"></div>
+      <div className="w-full h-screen bg-white flex items-center justify-center">
+        <motion.div className="w-[450px] h-[450px] relative">
+          <motion.img
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: false }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              visible: { y: 0, opacity: 1 },
+              hidden: { y: 100, opacity: 0 },
+            }}
+            src="/public/illustartion 5/5-.png"
+          />
+          <div className="w-[100px] h-[100px] absolute top-0 left-10">
+            <motion.div
+              ref={constraintsRef}
+              className="w-[100px] h-[100px]  relative  "
+            >
+              <motion.img
+                whileTap="press"
+                initial="hidden"
+                whileInView="visible"
+                className="absolute"
+                drag
+                dragConstraints={constraintsRef}
+                viewport={{ once: false }}
+                // whileHover={{ scale: 1.2 }}
+                transition={{ duration: 0.3, delay: 0.3 }}
+                variants={{
+                  visible: { y: 0, opacity: 1 },
+                  hidden: { y: 120, opacity: 0 },
+                }}
+                src="/public/illustartion 5/3-5.png"
+              />
+            </motion.div>
+          </div>
+          <motion.div className="w-[100px] h-[100px]  absolute top-0 right-10 ">
+            <motion.img
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false }}
+              transition={{ duration: 0.4, delay: 0.3 }}
+              variants={{
+                visible: { y: 0, opacity: 1 },
+                hidden: { y: 140, opacity: 0 },
+              }}
+              src="/public/illustartion 5/2-5.png"
+            />
+          </motion.div>
+          <div className="w-[100px] h-[100px] absolute bottom-0 right-24 ">
+            <motion.img
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false }}
+              transition={{ duration: 0.5, delay: 0.3 }}
+              variants={{
+                visible: { y: 0, opacity: 1 },
+                hidden: { y: 160, opacity: 0 },
+              }}
+              src="/public/illustartion 5/2-5.png"
+            />
+          </div>
+        </motion.div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
